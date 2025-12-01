f4dd553046456bea91962ebb85210ccb1d97deba
Yaron Lidgi <120150268+rlidgi@users.noreply.github.com>
2025-11-30 16:00:23 -0800

    redploing after adding scerests

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { AuthProvider } from './auth/AuthContext';
 
 
+
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
